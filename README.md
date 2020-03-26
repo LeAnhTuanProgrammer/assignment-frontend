60ecc2768cf39f1157ad7f1efacecc21ccba8a58
LeAnhTuanProgrammer <lleeaannhhttuuaann@gmail.com>
2020-03-27 01:49:22 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# assignmentfrontend
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/assignmentfrontend)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, ShopComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
