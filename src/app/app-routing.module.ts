36dd4874cc70f053b36e03a37ae78640d93e621d
LeAnhTuanProgrammer <lleeaannhhttuuaann@gmail.com>
2020-03-28 12:57:50 +0700

    4 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { ManagerComponent } from './manager/manager.component';
+import { ProductAddComponent } from './product-add/product-add.component';
 const routes: Routes = [
 {path:'home',component:HomeComponent},
   {path:'shop',component:ShopComponent},
   {path:'manager',component:ManagerComponent},
+  {path:'news',component:ProductAddComponent}
 ];
 
 @NgModule({
