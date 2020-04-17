c6ea12d1ea0afd967ad28c1c5829e12d4a67ce12
LeAnhTuanProgrammer <lleeaannhhttuuaann@gmail.com>
2020-04-18 01:59:31 +0700

    3 files modified, 6 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,23 @@ import { ManagerComponent } from './manager/manager.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductAddComponent } from './product-add/product-add.component';
-
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
-   { path: '', redirectTo: 'home', pathMatch: 'full'},
-{path:'home',component:HomeComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path:'home',component:HomeComponent},
   {path:'shop',component:ShopComponent},
+  {path:'admin',component:AdminComponent,
+  children:[
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path:'dashboard',component:DashboardComponent},
   {path:'manager',component:ManagerComponent},
   {path:'product/edit/:productID',component:ProductEditComponent},
   {path:'product/:productID',component:ProductDetailComponent},
   {path:'news',component:ProductAddComponent}
+]
+}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,15 @@ import { ManagerComponent } from './manager/manager.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductAddComponent } from './product-add/product-add.component';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, ShopComponent, ManagerComponent, ProductAddComponent, ProductEditComponent, ProductDetailComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, ShopComponent, ManagerComponent, ProductAddComponent, ProductEditComponent, ProductDetailComponent, AdminComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ],
    providers: [ProductService]
 })
